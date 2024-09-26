f2db13d8a8038e42e5b8ba2f670543b2e7448e43
Aantara21 <antarabaidya21@gmail.com>
2024-09-26 12:05:53 +0600

    third commit

--- part.js
+++ part.js
@@ -8,23 +8,66 @@ const card = give('card');
 
 // noyakhali donation 
 const myblnc = give('balance');
-const nJoma = give ('noyakhali-joma');
-const nbtn = give ('noyakhali-btn');
-const nInput = give ('noyakhali-input');
+const nJoma = give('noyakhali-joma');
+const nbtn = give('noyakhali-btn');
+const nInput = give('noyakhali-input');
 
-let amount = parseFloat (myblnc.innerText) ;
-console.log (typeof amount);
+// modal
+const modal = give('my_modal_5');
 
-nbtn.addEventListener('click', function(){
-    let noyabtnf = parseFloat (nJoma.innerText);
-const noyainputbtn = parseFloat(nInput.value);
-    noyabtnf = noyabtnf + noyainputbtn  ;
+// place
+const place = give('noyakhali')
+
+let amount = parseFloat(myblnc.innerText);
+console.log(typeof amount);
+
+nbtn.addEventListener('click', function () {
+
+    let noyakhaliAmount = parseFloat(nJoma.innerText);
+    const donationAmount = parseFloat(nInput.value);
+    if (isNaN(donationAmount) === true) {
+        alert('Invalid Input');
+    }
+    else if (donationAmount < 0) {
+        alert('Negetive Amount');
+    }
+    else if (donationAmount > amount) {
+        alert('Insufficient Balance');
+    }
+    else {
+        noyakhaliAmount = noyakhaliAmount + donationAmount;
+        nJoma.innerText = noyakhaliAmount;
+
+        const newBalance = amount - donationAmount;
+        myblnc.innerText = newBalance;
+
+        modal.showModal();
+        const items = document.createElement('div');
+        items.className = 'border rounded-2xl bg-white p-2';
+        items.innerHTML = `
+        <h2 class="font-bold text-lg">
+     ${donationAmount}Taka is Donated for ${place.innerText} 
+</h2>
+<p>Date : ${new Date()} </p>
+        `
+    hiddenHistry.appendChild(items);
+
+    
+    
+    }
+    nInput.value = '';
 })
 
 
 
+
+
+
+
+
+
 histry.addEventListener('click', function () {
-    histry.classList.add('font-bold','bg-green-200');
+    histry.classList.add('font-bold', 'bg-green-200');
     dontn.classList.remove('font-bold', 'bg-green-200');
     card.classList.add('hidden');
     hiddenHistry.classList.remove('hidden');
@@ -32,9 +75,9 @@ histry.addEventListener('click', function () {
 })
 
 dontn.addEventListener('click', function () {
-    dontn.classList.add('font-bold','bg-green-200');
+    dontn.classList.add('font-bold', 'bg-green-200');
     histry.classList.remove('font-bold', 'bg-green-200');
     card.classList.remove('hidden');
-    hiddenHistry.classList.add ('hidden');
+    hiddenHistry.classList.add('hidden');
 })
 
